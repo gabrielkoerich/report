643f0d7c0ae0d3587b2e5b3b7422163d890c4c86
Gabriel Koerich <gabriel@bulldesk.com.br>
2019-12-08 23:41:37 -0300

    Assert that report filtering works

--- tests/Feature/ReportTest.php
+++ tests/Feature/ReportTest.php
@@ -2,19 +2,79 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
 use App\Report;
+use App\Website;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ReportTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function testBasicTest()
-    {
-        $reports = Report::all();
+    use RefreshDatabase;
 
-        dd($reports);
+    /**
+     * Get the report data.
+     */
+    private function getReportData()
+    {
+        return [
+            'model' => Website::class,
+            'context' => [
+                [
+                    'field' => 'created_at',
+                    'operator' => '=',
+                    'query' => 'YEAR(NOW())',
+                ],
+                [
+                    'field' => 'domain',
+                    'operator' => 'ends',
+                    'query' => '.net|.com',
+                ]
+            ]
+        ];
+    }
+
+    public function testCreateReport()
+    {
+        $response = $this->post('/report', $this->getReportData());
+
+        $response->assertStatus(201);
+
+        $response = $this->get('/report');
+
+        $response->assertStatus(200);
+
+        $reports = json_decode($response->getContent());
+
+        $this->assertEquals($reports[0]->model, Website::class);
+    }
+
+    public function testShowReport()
+    {
+        $domains = [
+            ['domain' => 'testing.com'],
+            ['domain' => 'testing.net'],
+            ['domain' => 'testing.gov'],
+            ['domain' => 'testing.dev'],
+        ];
+
+        foreach ($domains as $domain) {
+            Website::create($domain);
+        }
+
+        $response = $this->post('/report', $this->getReportData());
+
+        $report = json_decode($response->getContent());
+
+        $response->assertStatus(201);
+
+        $response = $this->get('/report/' . $report->id);
+
+        $response->assertStatus(200);
+
+        $report = json_decode($response->getContent());
+
+        // Only .net and .com domains
+        $this->assertEquals($report->data[0]->domain, 'testing.com');
+        $this->assertEquals($report->data[1]->domain, 'testing.net');
     }
 }
